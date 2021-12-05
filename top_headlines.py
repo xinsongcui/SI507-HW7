42f3914c76238fc5a7dca5bf9373cc670bec59f3
Xinsong Cui <andrewtsui0@gmail.com>
2021-12-05 03:57:39 -0500

    Create

---
+++ top_headlines.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+from secrets import api_key
+import requests
+from requests import api
+
+app = Flask(__name__)
+query = {'api-key':api_key}
+url = "https://api.nytimes.com/svc/topstories/v2/technology.json?"
+response = requests.get(url, params=query)
+
+res = [x['title'] for x in response.json()['results'][0:5]]
+urls = [x['url'] for x in response.json()['results'][0:5]]
+
+@app.route('/')
+def hello_world():
+    return "<h1>Hello!</h1>"
+
+@app.route('/name/<username>')
+def hello(username):
+    return render_template('name.html', name=username)
+
+@app.route('/headlines/<username>')
+def headlines(username):
+    return render_template('headlines.html', headlines = res, name=username)
+
+@app.route('/links/<username>')
+def links(username):
+    return render_template('links.html', links = urls, headlines = res, name=username)
+
+# main driver function
+if __name__ == '__main__':
+    app.run()
